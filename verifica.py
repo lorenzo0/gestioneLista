dc8e5855ea250daa82c0109038225a543ade5957
Andrea Capobianco <andreacapobianco29@gmail.com>
2017-09-05 12:00:22 +0100

    verifica py 13:00

---
+++ verifica.py
@@ -0,0 +1,18 @@
+
+#def verifica():
+    if(os.path.isfile('lista.lst'))
+        #controlla gli atlri
+        if(os.path.exists(error))
+            
+        else
+            #crea la cartella error
+            mk_index(error)
+        if(os.path.exists(ok))
+            
+        else
+            #crea la cartella ok
+            mk_index(ok)
+    else
+        #stop
+
+#pass
